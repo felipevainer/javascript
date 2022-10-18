b64cdc5e130a2471027c1be145c77c520a1fbc77
Felipe Vainer <115707646+felipevainer@users.noreply.github.com>
2022-10-18 10:21:49 -0300

    Atualização

---
+++ cursojs/aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 35
+console.log(`Você tem ${idade} anos.`)
+if(idade < 16){
+    console.log('Não vota')
+}else if(idade < 18 || idade >= 75){
+        console.log('Voto Opcional')
+}else{
+        console.log('Voto obrigatorio!')
+    }
+    
+    
