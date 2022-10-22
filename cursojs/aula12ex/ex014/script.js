c9aba439925115c2e9118e7f660dddd55812b851
Felipe Vainer <115707646+felipevainer@users.noreply.github.com>
2022-10-22 09:05:26 -0300

    Atualização

--- cursojs/aula12ex/ex014/script.js
+++ cursojs/aula12ex/ex014/script.js
@@ -1,25 +1,40 @@
 function carregar(){
-    var msg = window.document.getElementById('msg')
-    var horadia = window.document.getElementById('horas')
-    var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora =  data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if(hora >= 0 && hora < 12){
-        //Bom dia!
-        horadia.innerHTML = 'Bom dia!'
-        img.src = 'manha.png'
-        window.document.body.style.background = '#e2cd9f'
-    }else if(hora >= 12 && hora <= 18){
-        //Boa tarde!
-        horadia.innerHTML = 'Boa tarde!'
-        img.src = 'tarde.png'
-        window.document.body.style.background = '#b9846f'
-    }else{
-        //Boa noite!
-        horadia.innerHTML = 'Boa noite!'
-        img.src = 'noite.png'
-        window.document.body.style.background = '#515154'
+    var hora = data.getHours()
+    var dia = data.getDay()
+    var img = document.getElementById('imagem')
+    var msg = document.getElementById('msg')
+    var bdia = document.getElementById('bdia')
+    if(dia == 0){
+        var d = 'Domindo'
+    }else if(dia == 1){
+        var d = 'Segunda-feira'
+    }else if(dia == 2){
+        var d = 'Terça-feira'
+    }else if(dia == 3){
+        var d = 'Quarta-feira'
+    }else if(dia == 4){
+        var d ='Quinta-feira'
+    }else if(dia == 5){
+        var d = 'Sexta-feira'
+    }else if(dia == 6){
+        var d = 'Sábado'
     }
-
+    msg.innerHTML = `Agora são ${hora} horas. Tenha uma ótima ${d}`
+    if(hora >=0 && hora < 12){
+        bdia.innerHTML = 'Bom dia!'
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9e'
+        msg.style.color= '#e2cd9e'
+    }else if(hora >= 12 && hora <= 18){
+        bdia.innerHTML = 'Boa tarde!'
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+        msg.style.color = '#b9846f'
+    }else{
+        bdia.innerHTML = 'Boa noite!'
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+        msg.style.color = '#515154'
+    }
 }
--- cursojs/aula12ex/ex015/script.js
+++ cursojs/aula12ex/ex015/script.js
@@ -1,49 +1,43 @@
 function verificar(){
    var data = new Date()
    var ano = data.getFullYear()
-   var fano = window.document.getElementById('txtano')
-   var res = window.document.querySelector('div#res')
-   if(fano.value.length == 0 || fano.value > ano){
-      window.alert('[ERRO] Verifique os dados e tente novamente!')
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if(fano.value.length == 0){
+      alert('[ERRO] Digite um valor númerico!')
+   }else if(Number(fano.value) < 1922 || Number(fano.value) > ano){
+      alert('[ERRO] Digite um valor númerico realista')
    }else{
-      var fsex = window.document.getElementsByName('radsex')
+      var fsex = document.getElementsByName('radsex')
       var idade = ano - Number(fano.value)
       var gênero = ''
       var img = document.createElement('img')
       img.setAttribute('id', 'foto')
       if(fsex[0].checked){
-         gênero = 'Homem'
+         gênero ='Homem'
          if(idade >= 0 && idade < 10){
-            //Criança
-            img.setAttribute('scr', 'foto-bebe-m.png')
+            img.setAttribute('src','foto-bebe-m.png')
          }else if(idade < 21){
-            //Jovem
-            img.setAttribute('src', 'foto-jovem-m.png')
+            img.setAttribute('src','foto-jovem-m.png')
          }else if(idade < 50){
-            //Adulto
             img.setAttribute('src', 'foto-adulto-m.png')
          }else{
-            //Idoso
             img.setAttribute('src', 'foto-idoso-m.png')
          }
       }else if(fsex[1].checked){
          gênero = 'Mulher'
          if(idade >= 0 && idade < 10){
-            //Criança
-            img.setAttribute('src', 'foto-bebe-f.png')
+            img.setAttribute('src','foto-bebe-f.png')
          }else if(idade < 21){
-            //Jovem
-            img.setAttribute('src', 'foto-jovem-f.png')
+            img.setAttribute('src','foto-jovem-f.png')
          }else if(idade < 50){
-            //Adulto
             img.setAttribute('src', 'foto-adulto-f.png')
          }else{
-            //Idoso
             img.setAttribute('src', 'foto-idoso-f.png')
          }
       }
       res.style.textAlign = 'center'
-      res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+      res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
       res.appendChild(img)
    }
 }
